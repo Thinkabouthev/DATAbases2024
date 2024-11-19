9b38516b0999c3fd0b0e03e6ae8e438ae8bc751b
Dariya <156985567+Thinkabouthev@users.noreply.github.com>
2024-11-19 23:18:14 +0500

    Add files via upload

---
+++ lab8.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE lab8; --1
+
+CREATE TABLE salesman(
+    salesman_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    city VARCHAR(255),
+    commission DECIMAL(5,2)
+);
+
+CREATE TABLE customers(
+    customer_id INT PRIMARY KEY,
+    cust_name VARCHAR(255),
+    city VARCHAR(255),
+    grade INT,
+    salesman_id INT REFERENCES salesman
+);
+
+CREATE TABLE orders(
+    ord_no INT PRIMARY KEY,
+    purch_amt DECIMAL(10,2),
+    ord_date DATE,
+    customer_id INT REFERENCES customers,
+    salesman_id INT REFERENCES salesman
+);
+
+INSERT INTO salesman(salesman_id, name, city, commission) VALUES
+(5001, 'James Hoog', 'New York', 0.15),
+(5002, 'Nail Knite', 'Paris', 0.13),
+(5005, 'Pit Alex', 'London', 0.11),
+(5006, 'Mc Lyon', 'Paris', 0.14),
+(5003, 'Lauson Hen', null, 0.12),
+(5007, 'Paul Adam', 'Rome', 0.13);
+
+INSERT INTO customers(customer_id, cust_name, city, grade, salesman_id) VALUES
+(3002, 'Nick Rimando', 'New York', 100, 5001),
+(3005, 'Graham Zusi', 'California', 200, 5002),
+(3001, 'Brad Guzan', 'London', null, 5005),
+(3004, 'Fabian Johns', 'Paris', 300, 5006),
+(3007, 'Brad Davis', 'New York', 200, 5001),
+(3009, 'Geoff Camero', 'Berlin', 100, 5003),
+(3008, 'Julian Green', 'London', 300, 5002);
+
+
+INSERT INTO orders(ord_no, purch_amt, ord_date, customer_id, salesman_id) VALUES
+(70001, 150.5, '2012-10-05', 3005, 5002),
+(70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001),
+(70004, 110.5, '2012-08-17', 3009, 5003),
+(70007, 948.5, '2012-09-10', 3005, 5002),
+(70005, 2400.6, '2012-07-27', 3007, 5001),
+(70008, 5760.0, '2012-09-10', 3002, 5001);
+
+
+CREATE ROLE junior_dev WITH login; --3
+
+CREATE VIEW salesman_ny AS --4
+    SELECT * FROM salesman WHERE city = 'New York';
+
+CREATE VIEW order_details AS --5
+    SELECT o.ord_no, o.purch_amt, o.ord_date, c.cust_name AS customer_name, s.name AS salesman_name
+    FROM orders o
+    JOIN salesman s ON o.salesman_id = s.salesman_id
+    JOIN customers c ON o.customer_id = c.customer_id;
+
+GRANT ALL PRIVILEGES ON order_details TO junior_dev;
+
+CREATE VIEW highestGradeCustomers AS --6
+    SELECT * FROM customers WHERE grade = (SELECT MAX(grade) FROM customers);
+
+GRANT SELECT ON highestGradeCustomers TO junior_dev;
+
+CREATE VIEW salesmanByCity AS --7
+    SELECT city, COUNT(*) as num_salesman
+    FROM salesman GROUP BY city;
+
+CREATE VIEW salesmanWithMultipleCustomers AS --8
+    SELECT s.salesman_id, s.name
+    FROM salesman s
+    JOIN customers c ON s.salesman_id = c.salesman_id
+    GROUP BY s.salesman_id, s.name
+    HAVING count(c.customer_id)>1;
+
+CREATE ROLE intern; --9
+GRANT junior_dev TO intern;
